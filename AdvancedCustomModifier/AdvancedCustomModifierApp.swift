3f8c8d582d324f95223769a81d5a66085548dd0c
Madhivananb <>
2024-08-06 22:17:43 +0530

    initial commit

---
+++ AdvancedCustomModifier/AdvancedCustomModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedCustomModifierApp.swift
+//  AdvancedCustomModifier
+//
+//  Created by Madhivanan on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedCustomModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
